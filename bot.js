95dc36febfafea37c9e51fe72d83be113b47209c
Gary Pang <gary.pang@gmail.com>
2017-06-18 17:00:12 -0400

    Remove boilerplate Botkit code
    Make Shakespeare Bot's language sound archive. (ex. "calleth" instead of
    "call.")

--- bot.js
+++ bot.js
@@ -1,68 +1,3 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-
-
-This is a sample Slack bot built with Botkit.
-
-This bot demonstrates many of the core features of Botkit:
-
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-
-# RUN THE BOT:
-
-  Get a Bot token from Slack:
-
-    -> http://my.slack.com/services/new/bot
-
-  Run your bot from the command line:
-
-    token=<MY TOKEN> node slack_bot.js
-
-# USE THE BOT:
-
-  Find your bot inside Slack to send it a direct message.
-
-  Say: "Hello"
-
-  The bot will reply "Hello!"
-
-  Say: "who are you?"
-
-  The bot will tell you its name, where it is running, and for how long.
-
-  Say: "Call me <nickname>"
-
-  Tell the bot your nickname. Now you are friends.
-
-  Say: "who am I?"
-
-  The bot will tell you your nickname, if it knows one for you.
-
-  Say: "shutdown"
-
-  The bot will ask if you are sure, and then shut itself down.
-
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-
-# EXTEND THE BOT:
-
-  Botkit has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
 /*
 if (!process.env.token) {
     console.log('Error: Specify token in environment');
@@ -82,30 +17,13 @@ var bot = controller.spawn({
     send_via_rtm: true // this enables bot.replyWithTyping()
 }).startRTM();
 
+
 controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
 
-    bot.replyWithTyping(message, 'Thou art a ' + shakespeare.random() + '!' + ' :middle_finger:');
+    bot.replyWithTyping(message, 'Thou art a ' + shakespeare.random() + '!');
 
-    /*bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        // name: 'robot_face',
-        name: 'middle_finger',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });*/
 });
+
 
 controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
     var name = message.match[1];
@@ -117,13 +35,12 @@ controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_men
         }
         user.name = name;
         controller.storage.users.save(user, function(err, id) {
-            // bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
             bot.reply(message, user.name + ', thou art a ' + shakespeare.random() + '!');
         });
     });
 });
 
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['what is my name', 'who am i', 'what is mine name'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
@@ -131,11 +48,11 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
         } else {
             bot.startConversation(message, function(err, convo) {
                 if (!err) {
-                    convo.say('I do not know thy name yet, thou ' + shakespeare.random() + '!');
+                    convo.say('I doth not knoweth thy name, thou ' + shakespeare.random() + '!');
                     convo.ask('What shall I call thee?', function(response, convo) {
-                        convo.ask('Thou want me to call you `' + response.text + '`?', [
+                        convo.ask('Thou wanteth me to calleth thee `' + response.text + '`?', [
                             {
-                                pattern: 'yes',
+                                pattern: 'yes', // aye
                                 callback: function(response, convo) {
                                     // since no further messages are queued after this,
                                     // the conversation will end naturally with status == 'completed'
@@ -143,7 +60,7 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
                                 }
                             },
                             {
-                                pattern: 'no',
+                                pattern: 'no', // nay
                                 callback: function(response, convo) {
                                     // stop the conversation. this will cause it to end with status == 'stopped'
                                     convo.stop();
@@ -164,7 +81,7 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
 
                     convo.on('end', function(convo) {
                         if (convo.status == 'completed') {
-                            bot.reply(message, 'Very well! I shall inscribe thy name upon my portfolio...');
+                            bot.reply(message, 'Very well! I shall inscribe thy name upon mine portfolio...');
 
                             controller.storage.users.get(message.user, function(err, user) {
                                 if (!user) {
@@ -174,11 +91,9 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
                                 }
                                 user.name = convo.extractResponse('nickname');
                                 controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'I shall call thee ' + user.name + ' from now on, thou ' + shakespeare.random() + '!');
+                                    bot.reply(message, 'I shalt calleth thee ' + user.name + ' from now on, thou ' + shakespeare.random() + '!');
                                 });
                             });
-
-
 
                         } else {
                             // this happens if the conversation ended prematurely for some reason
@@ -191,16 +106,15 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
     });
 });
 
-
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['shutdown', 'goodbye', 'bye', 'get lost'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     bot.startConversation(message, function(err, convo) {
 
-        convo.ask('Are you sure you want me to shutdown?', [
+        convo.ask('Art thou sure thou wanteth me to shutdown?', [
             {
                 pattern: bot.utterances.yes,
                 callback: function(response, convo) {
-                    convo.say('Bye!');
+                    convo.say('Farewell, thou ' + shakespeare.random() + '!');
                     convo.next();
                     setTimeout(function() {
                         process.exit();
@@ -219,8 +133,7 @@ controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function
     });
 });
 
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
+controller.hears(['uptime', 'identify yourself', 'identify thyself', 'who are you', 'who art thou', 'what is your name', 'what is thy name'],
     'direct_message,direct_mention,mention', function(bot, message) {
 
         var hostname = os.hostname();
@@ -229,28 +142,12 @@ controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your na
         bot.reply(message,
             ':robot_face: I am a bot named <@' + bot.identity.name +
              '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
     });
 
 controller.hears(['bite me', 'damn you', 'you suck'], 
     'direct_message,direct_mention,mention', function(bot, message) {
-        bot.reply(message, 'I bite my thumb at thee! :middle_finger: :middle_finger:');
+        bot.reply(message, 'I bite my thumb at thee! :middle_finger:');
     });
 
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}
+// TODO Say anything else to Shakespeare bot, and he will insult you with random insult!
+// TODO Make bot replyWithTyping to all user inputs. Make bot more life-like or "bot-like."
